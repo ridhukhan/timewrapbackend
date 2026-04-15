f307688f41b885da6547b118c39ddf418fe50a96
ridhukhan <prakritirshandhane@gmail.com>
2026-04-15 22:27:55 +0600

    update

--- config/nodemailer.js
+++ config/nodemailer.js
@@ -2,9 +2,7 @@ import nodemailer from "nodemailer";
 
 const getTransporter = () => {
   return nodemailer.createTransport({
-    host: "smtp.gmail.com",
-    port: 587,
-    secure: false,
+    service: "gmail", // সরাসরি সার্ভিস উল্লেখ করা ভালো
     auth: {
       user: process.env.EMAIL_USER,
       pass: process.env.EMAIL_PASS,

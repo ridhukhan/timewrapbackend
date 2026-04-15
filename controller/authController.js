867f696f6da5b21c84b2ee8b339dbb7d964b4219
ridhukhan <prakritirshandhane@gmail.com>
2026-04-15 21:28:30 +0600

    update

--- controller/authController.js
+++ controller/authController.js
@@ -11,7 +11,7 @@ const generateToken = (userId, res) => {
   res.cookie("token", token, {
     httpOnly: true,
     secure: process.env.NODE_ENV === "production",
-    sameSite: "strict",
+    sameSite: "none",
     maxAge: 7 * 24 * 60 * 60 * 1000,
   });
 };
